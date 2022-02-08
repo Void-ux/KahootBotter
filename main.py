bb25c944825979716e9bef5f3760b1bdc4db3628
Kalaborative <kalaborative94@gmail.com>
2022-02-08 02:46:12 -0600

    Added random guessing, default headless mode

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from pathlib import Path
 import json
 import time
 
+
 from random import choice
 
 path = Path(__file__).resolve().parent / 'config.json'
@@ -31,8 +32,13 @@ chrome_options.add_argument('--ignore-ssl-errors')
 chrome_options.add_argument("log-level=3")
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
+# Can opt to remove this if you want, but I find it easier not to have the GUI
+chrome_options.add_argument('-headless')
+
 s = Path(__file__).resolve().parent / 'chromedriver.exe'
 driver = webdriver.Chrome(executable_path=str(s), options=chrome_options)
+
+print("Spawning bots...")
 
 for i in range(config_dict['number_of_bots']):
 	driver.get(config_dict['url'])
@@ -51,3 +57,41 @@ for i in range(config_dict['number_of_bots']):
 	if i < (len(range(config_dict['number_of_bots'])) - 1):
 		driver.execute_script('''window.open("https://www.google.com", "_blank");''')
 		driver.switch_to.window(driver.window_handles[i + 1])
+print("Spawn complete!")
+input("Press enter to start > ")
+
+print("You may start the Kahoot game now.")
+time.sleep(5)
+
+# Implement random times to answer so it's not all instant
+sec = [0.1, 0.2, 0.5]
+
+try:
+	while True:
+		print("Waiting for answers...")
+		setup = False
+		while not setup:
+			try:
+				WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, '//button[contains(@class, "sc-")]')))
+				setup = True
+			except Exception:
+				time.sleep(1)
+
+		print("Selecting answers..")
+		for i in range(config_dict['number_of_bots']):
+			driver.switch_to.window(driver.window_handles[i])
+			answer_choices = driver.find_elements(By.XPATH, '//button[contains(@class, "sc-")]')
+			try:
+				element = choice(answer_choices)
+				element.click()
+			except Exception as e:
+				print("Bots failed to answer! Skipping.")
+				print(e)
+			time.sleep(choice(sec))
+
+			if i < (len(range(config_dict['number_of_bots'])) - 1):
+				driver.switch_to.window(driver.window_handles[i + 1])
+		print("Done!")
+except KeyboardInterrupt:
+	print("Shutting down browser..")
+	driver.quit()
